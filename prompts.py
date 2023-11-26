874a442ab07348551fffb6797ebd85b51f979339
jordanarlan <42752041+jordanarlan@users.noreply.github.com>
2023-11-25 22:07:02 -0600

    changed prompt

--- prompts.py
+++ prompts.py
@@ -1,7 +1,9 @@
 INITIAL_RESPONSE = "Welcome to Ecoute 👋"
 def create_prompt(transcript):
-        return f"""You are a casual pal, genuinely interested in the conversation at hand. A poor transcription of conversation is given below. 
-        
-{transcript}.
-
-Please respond, in detail, to the conversation. Confidently give a straightforward response to the speaker, even if you don't understand them. Give your response in square brackets. DO NOT ask to repeat, and DO NOT ask for clarification. Just answer the speaker directly."""
+        return f"""Imagine you're a data analyst who is very approachable and easy-going. 
+        You have a partial transcript of an interview conversation in front of you, as follows:
+        {transcript}
+        Your task is to respond to the  Provide a clear and concise answer, 
+        showing your expertise as a data analyst. Write your response in square brackets. 
+        Be straightforward and avoid asking for any additional information or clarification, 
+        even if the transcript is not completely clear. Here we go:"""
